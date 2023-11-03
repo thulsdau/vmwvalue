eeee3a42a51d09a96c764997a59c3912ca628c11
Tobias <thulsdau@gmail.com>
2023-11-03 17:58:25 +0100

    Added http headers and ticker symbol update

--- vmwvalue.py
+++ vmwvalue.py
@@ -8,7 +8,13 @@ try:
 except IndexError:
     OUTPUT_FILE="vmwvalue.html"
 
+cookies = "A3=d=AQABBPoYRWUCEP3JTdSkYZEZKT5Z21_XszgFEgABCAFeRmVuZeA9b2UBAiAAAAcI7hhFZUtBPDw&S=AQAAAnltKq6PYepskM9aCN0ZHNg; A1=d=AQABBPoYRWUCEP3JTdSkYZEZKT5Z21_XszgFEgABCAFeRmVuZeA9b2UBAiAAAAcI7hhFZUtBPDw&S=AQAAAnltKq6PYepskM9aCN0ZHNg; A1S=d=AQABBPoYRWUCEP3JTdSkYZEZKT5Z21_XszgFEgABCAFeRmVuZeA9b2UBAiAAAAcI7hhFZUtBPDw&S=AQAAAnltKq6PYepskM9aCN0ZHNg; EuConsent=CP0qjAAP0qjAAAOACKDEDdCgAAAAAAAAACiQAAAAAABhoAMAARBQEQAYAAiCgKgAwABEFAA; GUC=AQABCAFlRl5lbkIb8QQQ&s=AQAAAKnflepH&g=ZUUZBA; GUCS=AXlHnch7"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15"
+referer = "https://consent.yahoo.com/"
+headers = {'User-Agent': user_agent, 'Cookies': cookies, 'Referer':referer}
+
 regexp = '<fin-streamer class="Fw\\(b\\) Fz\\(36px\\) Mb\\(-4px\\) D\\(ib\\)" data-symbol="AVGO" data-test="qsp-price" data-field="regularMarketPrice" data-trend="none" data-pricehint="2" value="([0-9]+\\.[\\d]+)'
+regexp2 = '<div id="quote-market-notice" [^>]+><span>([^<]+)</span></div>'
 html_template = """\
 <!DOCTYPE html>
 <html lang="en">
@@ -22,8 +28,8 @@ html_template = """\
   <div style="display: flex; justify-content: center;">
     <div style="position: relative;">
       <div style="color: #40ff00; font-size: 25vw; font-weight: bold;">{0:.2f}</div>
-      <div style="color: grey;">AVGO: {1}</div>
-      <div style="color: grey;"> Updated: {2}</div>
+      <div style="color: grey;">AVGO: {1} ({3})</div>
+      <div style="color: grey;">Last data fetch: {2}</div>
       <div title="Formula: 142.50 * 0.479 + $AVGO * 0.252 * 0.521"; style="position:absolute; margin: 1px; bottom: 0; right: 0;">
         <a href="https://github.com/thulsdau/vmwvalue/" style="text-decoration: none; color: darkgrey">?&#x20DD;</a>
       </div>
@@ -38,25 +44,32 @@ gmt_time = time.strftime('%d/%b/%Y %H:%M:%S GMT', time.gmtime())
 print(gmt_time,"Fetching new data.")
 
 try:
-    resp = requests.get('https://finance.yahoo.com/quote/AVGO?p=AVGO&.tsrc=fin-srch')
+    resp = requests.get('https://finance.yahoo.com/quote/AVGO',headers=headers)
 except Exception as e:
     print(gmt_time,"HTTP Exception:",str(e))
     sys.exit(1)
 
 if resp.status_code == 200:
     m = re.search(regexp,resp.text)
-    if m:
+    m2 = re.search(regexp2,resp.text)
+    if m and m2:
+        yf_debug = 'yf_debug.html'
+        with open(yf_debug,'w') as logfile:
+            logfile.write(resp.text)
         avgo = m.group(1)
         vmw = 142.50 * 0.479 + float(avgo) * 0.252 * 0.521
+        ticker_updated = m2.group(1)
         with open('avgo.html.tmp','w') as output:
-            output.write(html_template.format(vmw,avgo,gmt_time))
+            output.write(html_template.format(vmw,avgo,gmt_time,ticker_updated))
         os.rename('avgo.html.tmp',OUTPUT_FILE)
+        print(gmt_time,"Success: VMW value is now {0} (AVGO {1}).".format(str(vmw),str(avgo)))
     else:
         html_log = 'html_log.txt.{}'.format(time.time())
-        print(gmt_time,"Regexp Error: No match! See raw html in:",html_log)
+        if not m:
+            print(gmt_time,"Regexp Error: No match! See raw html in:",html_log)
+        if not m2:
+            print(gmt_time,"Regexp2 Error: No match! See raw html in:",html_log)
         with open(html_log,'w') as logfile:
             logfile.write(resp.text)
 else:
     print(gmt_time,"HTTP Error:",str(resp.status_code))
-
-print(gmt_time,"Success: VMW value is now {0}.".format(str(vmw)))
